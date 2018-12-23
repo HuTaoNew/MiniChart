d979d622e55d94de434b80aa54ef269d6bcf237f
Hutao <hutao_hn@163.com>
2018-12-23 12:14:26 +0800

    完成注册功能

--- routes/register.js
+++ routes/register.js
@@ -6,4 +6,44 @@ router.get('/', function(req, res, next) {
     res.render('register', { title: 'Register' });
 });
 
+// 定义用户数组
+let users = [];
+
+router.post('/', function(req, res, next) {
+    // 获取前端传过来的数据
+    let userName = req.body.userName;
+    let loginPwd = req.body.loginPwd;
+
+    // 创建用户对象
+    let regUser = {
+        userName,
+        loginPwd
+    }
+
+    // 验证用户是否已经注册
+    let user = isReg(regUser);
+    if (user === null || user === undefined){
+        users.push(regUser);
+        //res.send("用户注册成功！");
+
+        // 重定向到登陆
+        res.redirect("/login");
+    } else {
+        res.send("用户已注册！");
+    }
+
+});
+
+/*
+ * 判断用户是否已经注册的函数
+ */
+let isReg = (usrObj) => {
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        if (user.userName === usrObj.userName) {
+            return user;
+        }
+    }
+}
+
 module.exports = router;
